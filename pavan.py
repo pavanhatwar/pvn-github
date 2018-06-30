24e6834bbf6b9e92f556030e7f48f01467f8a86a
pavanhatwar <40316093+pavanhatwar@users.noreply.github.com>
2018-06-30 22:14:58 +0530

    Update pavan.py

--- pavan.py
+++ pavan.py
@@ -1,15 +1,8 @@
 import time 
-
 time.sleep (2)
-
 input = raw_input("Please Type Anything  :")
-
 time.sleep (3)
-
-
 print 'hello,',input
 time.sleep (1)
-
 print 'how u doing ??'
-
 time.sleep (2)
